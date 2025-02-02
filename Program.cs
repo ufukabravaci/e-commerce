2a1034c67e6ef97d31998840712fe30b3290394c
Ufuk ABRAVACI <108156229+ufukabravaci@users.noreply.github.com>
2025-02-02 12:52:05 +0300

    small update

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            int a = 10;
+            int a = 20;
             Console.WriteLine("a: " + a);
         }
     }
