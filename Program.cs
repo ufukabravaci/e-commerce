56e518dd28d85d3a078cc59e0c6269ce6f6458c5
Ufuk ABRAVACI <108156229+ufukabravaci@users.noreply.github.com>
2025-02-02 12:50:42 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace e_commerce
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int a = 10;
+            Console.WriteLine("a: " + a);
+        }
+    }
+}
